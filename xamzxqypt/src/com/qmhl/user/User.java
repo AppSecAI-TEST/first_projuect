9a19a5076b838ad46bf9caaffe016e8654f73008
shendx520 <33197520@qq.com>
2017-08-11 14:23:10 +0800

    test modify branch

--- xamzxqypt/src/com/qmhl/user/User.java
+++ xamzxqypt/src/com/qmhl/user/User.java
@@ -12,4 +12,64 @@ public class User {
 	 
 	 
 	 private String address;
+	 
+	 
+	 private String tel;
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getTel() {
+		return tel;
+	}
+
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", age=" + age + ", address=" + address + ", tel=" + tel
+				+ "]";
+	}
 }
