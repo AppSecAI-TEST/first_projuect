a88c302ba4bb912155e41c4e7fa8f0d77f003553
shendx520 <33197520@qq.com>
2017-08-11 14:14:07 +0800

    eeeee

--- xamzxqypt/src/com/qmhl/user/User.java
+++ xamzxqypt/src/com/qmhl/user/User.java
@@ -12,4 +12,58 @@ public class User {
 	 
 	 
 	 private String address;
+	 
+	 
+	 private String phone;
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	 
 }
